9c9602e7b92a1c270fabaa48889a636617e9fd6f
Mahan <91977844+MahanHM18@users.noreply.github.com>
2022-10-11 21:27:57 +0330

    Init

---
+++ QuizApp/Quiz/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace Quiz
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.A1 = new System.Windows.Forms.TextBox();
+            this.A2 = new System.Windows.Forms.TextBox();
+            this.A3 = new System.Windows.Forms.TextBox();
+            this.FinishQuizBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(436, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Question1";
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(436, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Question2";
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 140);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(436, 23);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Question3";
+            // 
+            // A1
+            // 
+            this.A1.Location = new System.Drawing.Point(12, 68);
+            this.A1.Name = "A1";
+            this.A1.Size = new System.Drawing.Size(100, 20);
+            this.A1.TabIndex = 3;
+            // 
+            // A2
+            // 
+            this.A2.Location = new System.Drawing.Point(12, 117);
+            this.A2.Name = "A2";
+            this.A2.Size = new System.Drawing.Size(100, 20);
+            this.A2.TabIndex = 4;
+            // 
+            // A3
+            // 
+            this.A3.Location = new System.Drawing.Point(12, 166);
+            this.A3.Name = "A3";
+            this.A3.Size = new System.Drawing.Size(100, 20);
+            this.A3.TabIndex = 5;
+            // 
+            // FinishQuizBtn
+            // 
+            this.FinishQuizBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FinishQuizBtn.Location = new System.Drawing.Point(138, 198);
+            this.FinishQuizBtn.Name = "FinishQuizBtn";
+            this.FinishQuizBtn.Size = new System.Drawing.Size(158, 59);
+            this.FinishQuizBtn.TabIndex = 6;
+            this.FinishQuizBtn.Text = "Finish";
+            this.FinishQuizBtn.UseVisualStyleBackColor = true;
+            this.FinishQuizBtn.Click += new System.EventHandler(this.FinishQuizBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(460, 276);
+            this.Controls.Add(this.FinishQuizBtn);
+            this.Controls.Add(this.A3);
+            this.Controls.Add(this.A2);
+            this.Controls.Add(this.A1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox A1;
+        private System.Windows.Forms.TextBox A2;
+        private System.Windows.Forms.TextBox A3;
+        private System.Windows.Forms.Button FinishQuizBtn;
+
+        #endregion
+    }
+}
---
+++ QuizApp/Quiz/Form1.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Quiz
+{
+    public partial class Form1 : Form
+    {
+        private string[] Questions =
+        {
+            "All statments must end with a ____",
+            "What result is returned by 2 + 3 * 4 / 2?",
+            "What keyword is used to declare an integer?",
+            "What result is returned by (3 > 2) & (4 < 2)?",
+            "what do duble slashes (//) precode?"
+        };
+
+        Random rnd = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            label1.Text = Questions[rnd.Next(0, Questions.Length)];
+
+            label2.Text = Questions[rnd.Next(0, Questions.Length)];
+
+            label3.Text = Questions[rnd.Next(0, Questions.Length)];
+
+            CheckQuestions();
+        }
+
+
+        private void CheckQuestions()
+        {
+            while (label2.Text == label1.Text)
+            {
+                label2.Text = Questions[rnd.Next(0, Questions.Length)];
+            }
+
+            while (label3.Text == label2.Text || label3.Text == label1.Text)
+            {
+                label3.Text = Questions[rnd.Next(0, Questions.Length)];
+            }
+        }
+
+
+        private bool GetCorrectAnsewr(int index)
+        {
+            if (index == 1)
+            {
+                switch (label1.Text)
+                {
+                    case "All statments must end with a ____":
+                    {
+                        if (A1.Text == ";")
+                        {
+                            return true;
+                            break;
+                        }
+
+                        return false;
+                    }
+                    case "What result is returned by 2 + 3 * 4 / 2?":
+                    {
+                        if (A1.Text == "8")
+                        {
+                            return true;
+                            break;
+                        }
+
+                        return false;
+                    }
+                    case "What keyword is used to declare an integer?":
+                    {
+                        if (A1.Text == "in")
+                        {
+                            return true;
+                            break;
+                        }
+
+                        return false;
+                        break;
+                    }
+                    case "What result is returned by (3 > 2) & (4 < 2)?":
+                    {
+                        if (A1.Text == "false")
+                        {
+                            return true;
+                            break;
+                        }
+
+                        return false;
+                        break;
+                    }
+                    case "what do double slashes (//) precode?":
+                    {
+                        if (A1.Text == "comments")
+                        {
+                            return true;
+                            break;
+                        }
+
+                        return false;
+                        break;
+                    }
+
+                    default:
+                        break;
+                }
+            }
+            else if (index == 2)
+            {
+                switch (label2.Text)
+                {
+                    case "All statments must end with a ____":
+                    {
+                        if (A2.Text == ";")
+                        {
+                            return true;
+                            break;
+                        }
+
+                        return false;
+                    }
+                    case "What result is returned by 2 + 3 * 4 / 2?":
+                    {
+                        if (A2.Text == "8")
+                        {
+                            return true;
+                            break;
+                        }
+
+                        return false;
+                    }
+                    case "What keyword is used to declare an integer?":
+                    {
+                        if (A2.Text == "in")
+                        {
+                            return true;
+                            break;
+                        }
+
+                        return false;
+                        break;
+                    }
+                    case "What result is returned by (3 > 2) & (4 < 2)?":
+                    {
+                        if (A2.Text == "false")
+                        {
+                            return true;
+                            break;
+                        }
+
+                        return false;
+                        break;
+                    }
+                    case "what do duble slashes (//) precode?":
+                    {
+                        if (A2.Text == "comments")
+                        {
+                            return true;
+                            break;
+                        }
+
+                        return false;
+                        break;
+                    }
+
+                    default:
+                        break;
+                }
+            }
+            else if (index == 3)
+            {
+                switch (label3.Text)
+                {
+                    case "All statments must end with a ____":
+                    {
+                        if (A3.Text == ";")
+                        {
+                            return true;
+                            break;
+                        }
+
+                        return false;
+                    }
+                    case "What result is returned by 2 + 3 * 4 / 2?":
+                    {
+                        if (A3.Text == "8")
+                        {
+                            return true;
+                            break;
+                        }
+
+                        return false;
+                    }
+                    case "What keyword is used to declare an integer?":
+                    {
+                        if (A3.Text == "in")
+                        {
+                            return true;
+                            break;
+                        }
+
+                        return false;
+                        break;
+                    }
+                    case "What result is returned by (3 > 2) & (4 < 2)?":
+                    {
+                        if (A3.Text == "false")
+                        {
+                            return true;
+                            break;
+                        }
+
+                        return false;
+                        break;
+                    }
+                    case "what do duble slashes (//) precode?":
+                    {
+                        if (A3.Text == "comments")
+                        {
+                            return true;
+                            break;
+                        }
+
+                        return false;
+                        break;
+                    }
+
+                    default:
+                        break;
+                }
+            }
+
+
+            return false;
+        }
+
+        private void FinishQuizBtn_Click(object sender, EventArgs e)
+        {
+            string[] message =
+            {
+                GetCorrectAnsewr(1) ? "correct" : "incorrect",
+                GetCorrectAnsewr(2) ? "correct" : "incorrect",
+                GetCorrectAnsewr(3) ? "correct" : "incorrect",
+            };
+            MessageBox.Show(message[0] + "\n" + message[1] + "\n" + message[2] + "\n","Result");
+        }
+    }
+}
